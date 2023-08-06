dca7ddc12c1d8224152d5ae8373c07e26264a31b
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-08-06 19:07:24 +0530

    big fixes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ function Header() {
               d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
             />
           </svg>
-          <span className="font-bold text-xl">AirBnB</span>
+          <span className="font-bold text-xl">airBnB</span>
         </Link>
         <div className="flex gap-2 border border-gray-400 rounded-full py-2 px-4 shadow-md shadow-gray-300 ">
           <div className="flex items-center ">Anywhere</div>
@@ -63,7 +63,7 @@ function Header() {
             />
           </svg>
           <Link
-            to={user ? "account" : "/login"}
+            to={user ? "/account" : "/login"}
             className="bg-gray-500 text-white rounded-full p-1"
           >
             <svg
