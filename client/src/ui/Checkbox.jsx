c499f03d8526fdb0e5115f314293913355a87100
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-08-08 22:51:10 +0530

    logic for auto filling the edit form

--- client/src/ui/Checkbox.jsx
+++ client/src/ui/Checkbox.jsx
@@ -1,12 +1,21 @@
 import { Controller, useForm } from "react-hook-form";
 
-function Checkbox({ label, value, control, icon }) {
+function Checkbox({ label, value, control, icon, apartment, name }) {
   return (
     <div className="flex items-center gap-1">
       <Controller
         name={value}
         control={control}
-        render={({ field }) => <input type="checkbox" id={value} {...field} />}
+        render={({ field }) => (
+          <input
+            type="checkbox"
+            checked={
+              apartment?.perks?.filter((perk) => perk === name).length > 0
+            }
+            id={value}
+            {...field}
+          />
+        )}
       />
       <label
         htmlFor={value}
